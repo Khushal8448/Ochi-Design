1b2bf764d8e01b520b0185538dba70f13ec5ded0
Khushal Mali <khushalmali8448@gmail.com>
2024-07-11 23:36:04 +0530

    marquee section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="w-full h-screen text-white">
+    <div className="w-full min-h-screen text-white bg-zinc-900">
       <Navbar />
       <LandingPage />
       <Marquee />
---
+++ src/components/Marquee.jsx
@@ -0,0 +1,17 @@
+const Marquee = () => {
+  return (
+    <div className="w-full py-24 rounded-t-2xl bg-[#004D43]">
+      <div className="text border-t-2 border-b-2 gap-10 border-zinc-300 flex whitespace-nowrap overflow-hidden">
+        <h1 className="text-[28vw] font-FoundersGrotesk leading-[.8] mt-[-6vh] font-semibold uppercase ">
+          we are ochi
+        </h1>
+
+        <h1 className="text-[28vw] font-FoundersGrotesk leading-[.8] mt-[-6vh] font-semibold uppercase ">
+          we are ochi
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Marquee;
