2f6f92207e9a72de90239e9656d7749e72047404
Khushal Mali <khushalmali8448@gmail.com>
2024-07-11 18:46:23 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div className="w-full h-screen bg-zinc-900 text-white">App</div>;
+  return (
+    <div className="w-full h-screen text-white">
+      <Navbar />
+      <LandingPage />
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        FoundersGrotesk: ["FoundersGrotesk", "sans-serif"],
+        NeueMontreal: ["NeueMontreal", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
