ad6490f5e2cc64a5696dc74ffafa2223d28aca17
Khushal Mali <khushalmali8448@gmail.com>
2024-07-12 22:09:39 +0530

    landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,8 +2,8 @@ import { MdArrowOutward } from "react-icons/md";
 
 const LandingPage = () => {
   return (
-    <div className="w-full h-screen bg-zinc-900 pt-1">
-      <div className="textstructure mt-40 px-20">
+    <div className="w-full h-screen pt-1">
+      <div className="textstructure mt-40 px-12">
         {["WE CREATE", "EYE-OPENING", "PRESENTATIONS"].map((item, index) => (
           <div
             key={index}
@@ -18,7 +18,7 @@ const LandingPage = () => {
           </div>
         ))}
       </div>
-      <div className="border-t-[1px] flex justify-between items-center py-5 px-20 border-zinc-800 mt-20">
+      <div className="border-t-[1px] flex max-lg:flex-col max-lg:items-start max-lg:gap-8 justify-between items-center py-5 px-12 border-zinc-800 mt-20">
         {[
           "For public and private companies",
           "From the first pitch to IPO",
@@ -31,10 +31,10 @@ const LandingPage = () => {
           </p>
         ))}
         <div className="start flex gap-1.5 items-center">
-          <div className="px-5 py-2 border-[1px] font-NeueMontreal  font-light border-zinc-300 uppercase leading-none cursor-pointer rounded-full">
+          <div className="px-5 py-2 border-[1px] font-NeueMontreal  font-light border-zinc-900 uppercase leading-none cursor-pointer rounded-full">
             Start the project
           </div>
-          <div className="w-8 h-8 flex items-center justify-center border-[2px] border-zinc-300  rounded-full">
+          <div className="w-8 h-8 flex items-center justify-center border-[1px] border-zinc-900  rounded-full">
             <MdArrowOutward className="text-lg" />
           </div>
         </div>
