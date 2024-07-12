2904896162bb741a2d592820f90ce78637c4493b
Khushal Mali <khushalmali8448@gmail.com>
2024-07-12 22:08:35 +0530

    marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -2,22 +2,53 @@ import { motion } from "framer-motion";
 
 const Marquee = () => {
   return (
-    <div className="w-full py-24 rounded-t-2xl bg-[#004D43]">
-      <div className="text border-t-2 border-b-2 gap-10 border-zinc-300 flex whitespace-nowrap overflow-hidden">
+    <div className="w-full text-white relative py-28 rounded-t-2xl bg-[#004D43]">
+      <div className="text gap-10 flex whitespace-nowrap overflow-hidden">
+        <div className="absolute top-24 w-full">
+          <svg width="100%" height="1">
+            <line
+              x1="0"
+              y1="0"
+              x2="100%"
+              y2="0"
+              stroke="#558882"
+              strokeWidth="100"
+            />
+          </svg>
+        </div>
+        <div className="absolute bottom-24 w-full">
+          <svg width="100%" height="1">
+            <line
+              x1="0"
+              y1="0"
+              x2="100%"
+              y2="0"
+              stroke="#558882"
+              strokeWidth="100"
+            />
+          </svg>
+        </div>
         <motion.h1
           initial={{ x: 0 }}
           animate={{ x: "-100%" }}
           transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
-          className="text-[28vw] font-FoundersGrotesk leading-[.8] mt-[-6vh] font-semibold uppercase "
+          className="text-[28vw] font-FoundersGrotesk leading-[.8] mt-[-3.4vw] font-semibold uppercase "
         >
           we are ochi
         </motion.h1>
-
         <motion.h1
           initial={{ x: 0 }}
           transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
           animate={{ x: "-100%" }}
-          className="text-[28vw] font-FoundersGrotesk leading-[.8] mt-[-6vh] font-semibold uppercase "
+          className="text-[28vw] font-FoundersGrotesk leading-[.8] mt-[-3.4vw] font-semibold uppercase "
+        >
+          we are ochi
+        </motion.h1>
+        <motion.h1
+          initial={{ x: 0 }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          animate={{ x: "-100%" }}
+          className="text-[28vw] font-FoundersGrotesk leading-[.8] mt-[-3.4vw] font-semibold uppercase "
         >
           we are ochi
         </motion.h1>
