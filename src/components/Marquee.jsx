cead7ce556dc0175753b8e4083f4cf63b6a19508
Khushal Mali <khushalmali8448@gmail.com>
2024-07-12 08:45:19 +0530

    marquee animation

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,14 +1,26 @@
+import { motion } from "framer-motion";
+
 const Marquee = () => {
   return (
     <div className="w-full py-24 rounded-t-2xl bg-[#004D43]">
       <div className="text border-t-2 border-b-2 gap-10 border-zinc-300 flex whitespace-nowrap overflow-hidden">
-        <h1 className="text-[28vw] font-FoundersGrotesk leading-[.8] mt-[-6vh] font-semibold uppercase ">
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[28vw] font-FoundersGrotesk leading-[.8] mt-[-6vh] font-semibold uppercase "
+        >
           we are ochi
-        </h1>
+        </motion.h1>
 
-        <h1 className="text-[28vw] font-FoundersGrotesk leading-[.8] mt-[-6vh] font-semibold uppercase ">
+        <motion.h1
+          initial={{ x: 0 }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          animate={{ x: "-100%" }}
+          className="text-[28vw] font-FoundersGrotesk leading-[.8] mt-[-6vh] font-semibold uppercase "
+        >
           we are ochi
-        </h1>
+        </motion.h1>
       </div>
     </div>
   );
